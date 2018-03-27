7f1fe75093d2c74e53b54f4228bcd514f18345e2
Jupplsix <37137000+Jupplsix@users.noreply.github.com>
2018-03-28 08:43:09 +1300

    Edited JavaScript

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 var landing = document.querySelector("#landing");
 
-function resizeLanding() {
-	var width = window.innerWidth;
-	if(width >= 680) {
-		var height = window.innerHeight;
-		height = height + "px"
-	}
-}
+var section1Button = document.querySelector("#buttonToSection1");
+var section2Button = document.querySelector("#buttonToSection2");
+var section3Button = document.querySelector("#buttonToSection3");
+var section4Button = document.querySelector("#buttonToSection4");
+
+
+
+// Resize Landing to Fit Screen
 
 function resizeLandingLoad() {
 	height = window.innerHeight;
@@ -14,5 +15,48 @@ function resizeLandingLoad() {
 	landing.style.height = height;
 }
 
+// Scroll To Sections
+
+function scrollToSection1 () {
+ 	var section = document.querySelector("#section1");
+	section.scrollIntoView({
+			behavior: "smooth",
+			block: "start",
+			inline: "nearest"
+		});
+};
+
+function scrollToSection2 () {
+ 	var section = document.querySelector("#section2");
+	section.scrollIntoView({
+			behavior: "smooth",
+			block: "start",
+			inline: "nearest"
+		});
+};
+
+function scrollToSection3 () {
+ 	var section = document.querySelector("#section3");
+	section.scrollIntoView({
+			behavior: "smooth",
+			block: "start",
+			inline: "nearest"
+		});
+};
+
+function scrollToSection4 () {
+ 	var section = document.querySelector("#section4");
+	section.scrollIntoView({
+			behavior: "smooth",
+			block: "start",
+			inline: "nearest"
+		});
+};
+
 window.addEventListener('resize', resizeLanding); 
 window.addEventListener('load', resizeLandingLoad);
+
+section1Button.addEventListener('click', scrollToSection1);
+section2Button.addEventListener('click', scrollToSection2);
+section3Button.addEventListener('click', scrollToSection3);
+section4Button.addEventListener('click', scrollToSection4);
