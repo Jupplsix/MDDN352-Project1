a3a1e28d69457c543d547fd1b2d93ccf387d3fde
Jupplsix <37137000+Jupplsix@users.noreply.github.com>
2018-03-28 14:14:47 +1300

    Update to Original

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// Get Required Elements from HTML
-
 var landing = document.querySelector("#landing");
 
 var section1Button = document.querySelector("#buttonToSection1");
@@ -19,20 +17,45 @@ function resizeLandingLoad() {
 
 // Scroll To Sections
 
-function scrollToSection1 (x) {
- 	var section = document.querySelector("#section" + x);
+function scrollToSection1 () {
+ 	var section = document.querySelector("#section1");
 	section.scrollIntoView({
-		behavior: "smooth",
-		block: "center",
-		inline: "nearest"
-	});
+			behavior: "smooth",
+			block: "start",
+			inline: "nearest"
+		});
 };
 
-// Required Event Listeners
+function scrollToSection2 () {
+ 	var section = document.querySelector("#section2");
+	section.scrollIntoView({
+			behavior: "smooth",
+			block: "start",
+			inline: "nearest"
+		});
+};
+
+function scrollToSection3 () {
+ 	var section = document.querySelector("#section3");
+	section.scrollIntoView({
+			behavior: "smooth",
+			block: "start",
+			inline: "nearest"
+		});
+};
+
+function scrollToSection4 () {
+ 	var section = document.querySelector("#section4");
+	section.scrollIntoView({
+			behavior: "smooth",
+			block: "start",
+			inline: "nearest"
+		});
+};
 
 window.addEventListener('load', resizeLandingLoad);
 
-section1Button.addEventListener('click', function(){ scrollToSection1(1) });
-section2Button.addEventListener('click', function(){ scrollToSection1(2) });
-section3Button.addEventListener('click', function(){ scrollToSection1(3) });
-section4Button.addEventListener('click', function(){ scrollToSection1(4) });
+section1Button.addEventListener('click', scrollToSection1);
+section2Button.addEventListener('click', scrollToSection2);
+section3Button.addEventListener('click', scrollToSection3);
+section4Button.addEventListener('click', scrollToSection4);
